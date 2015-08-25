d590907170ac00b4bf02849017ce0d2e78471e0d
861733294 <861733294@qq.com>
2015-08-25 17:22:27 +0800

    add newCode

--- GitHudDemo/ViewController.m
+++ GitHudDemo/ViewController.m
@@ -16,6 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSLog(@"ok");
+    
+    NSLog(@"fdsa");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
